9a864f6ee23b360cec0e6152aaa8cc617a7cbc72
Samiya <skmcintosh20@gmail.com>
2022-04-29 13:44:24 -0700

    created portfolio assets component

--- screens/Portfolio.js
+++ screens/Portfolio.js
@@ -1,11 +1,11 @@
-import {View, Text} from "react-native";
+import {View, Text, FlatList} from "react-native";
 import React from "react";
 import {MainLayout} from "./Main";
 export default function Portfolio() {
 	return (
 		<MainLayout>
 			<View>
-				<Text>Portfolio</Text>
+				<FlatList />
 			</View>
 		</MainLayout>
 	);
